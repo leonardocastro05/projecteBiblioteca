a627e588b9cc58fe283a793898c89e6bcc6f3eab
rfilimon <rebeca.filimon@insbaixcamp.cat>
2025-05-14 14:44:07 +0200

    He tabulat la classe GestorBiblioteca.java

--- GestorBiblioteca.java
+++ GestorBiblioteca.java
@@ -1,23 +1,27 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 public class GestorBiblioteca {
-private List<Prestec> prestecs;
-public GestorBiblioteca() {
-this.prestecs = new ArrayList<>();
-}
-public void prestarLlibre(Usuari usuari, Llibre llibre) {
-if (!llibre.esPrestat()) {
-llibre.prestar();
-Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
-prestecs.add(prestec);
-usuari.afegirLlibre(llibre);
-System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
-} else {
-System.out.println("Aquest llibre ja està prestat.");
-}
-}
-public void controlarEstoc(){
-    
-}
+    private List<Prestec> prestecs;
+
+    public GestorBiblioteca() {
+        this.prestecs = new ArrayList<>();
+    }
+
+    public void prestarLlibre(Usuari usuari, Llibre llibre) {
+        if (!llibre.esPrestat()) {
+            llibre.prestar();
+            Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
+            prestecs.add(prestec);
+            usuari.afegirLlibre(llibre);
+            System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
+        } else {
+            System.out.println("Aquest llibre ja està prestat.");
+        }
+    }
+
+    public void controlarEstoc() {
+
+    }
 }
