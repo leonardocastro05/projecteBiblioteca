126c945a250ffb78b6952923a319b11f7d20f2f6
rfilimon <rebeca.filimon@insbaixcamp.cat>
2025-05-14 14:20:50 +0200

    Canvi temporal de nom per forçar el canvi de majúscules

--- temp.java
+++ temp.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class temp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Biblioteca biblioteca = new Biblioteca();
